b42b0ae47271153eb374f6993723622726ce8b2e
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-19 00:04:45 +0700

    Add handler for cancel command

--- Echo bot/Guess_the_number.py
+++ Echo bot/Guess_the_number.py
@@ -11,19 +11,19 @@ BOT_TOKEN = ""
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-#Количество попыток, доступных пользователю
+# Количество попыток, доступных пользователю
 ATTEMPTS = 5
-user = {"in_game": False,
-        "secret_number": None,
-        "attempt": None,
-        "total_games": 0,
-        "wins": 0}
+users = {}
 
 # Функция возвращения рандомного целого числа от 1 до 100
+
+
 def get_random_numer() -> int:
     return random.randint(1, 100)
 
 # Хендлер, вызываемый при команде /help
+
+
 @dp.message(CommandStart())
 async def procces_start_command(message: Message):
     await message.answer(
@@ -31,7 +31,19 @@ async def procces_start_command(message: Message):
         "Давайте сыграем в игру 'Угадай число'!"
         "Чтобы узнать правила игры и посмотреть список доступных команд, отправьте команду /help."
     )
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {
+            'in_game': False,
+            'secret_number': None,
+            'attempts': None,
+            'total_games': 0,
+            'wins': 0
+        }
+
 # Хендлер, вызываемый при команде /help
+
+
+@dp.message(Command(commands='help'))
 async def procces_help_command(message: Message):
     await message.answer(
         f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
@@ -40,3 +52,16 @@ async def procces_help_command(message: Message):
         f'игры и список команд\n/cancel - выйти из игры\n'
         f'/stat - посмотреть статистику\n\nДавай сыграем?'
     )
+
+
+@dp.message(Command(commands='cancel'))
+async def procces_cancel_command(message: Message):
+    if users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in)game'] = False
+        await message.answer(
+            'Вы вышли из игры. Если захотите сыграть снова, дайте знать.'
+        )
+    else:
+        await message.answer(
+            'Мы еще не начали игру. Может, попробуем сыграть?'
+        )
